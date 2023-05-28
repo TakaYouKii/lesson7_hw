f3e523ea896f1a98a3d9c5fb58d6787763080564
TakaYouKii <avwro1708@gmaol.cpm>
2023-05-29 00:19:26 +0600

    hw 7

---
+++ app/src/main/java/com/example/lesson7/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.lesson7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Boolean isOperationClick;
+    private Double first, second, result;
+    private String operation = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text_view);
+
+
+    }
+
+    public void onNumberClick(View view){
+        if(view.getId() == R.id.btn_1){
+            setNumber("1");
+        }
+        else if (view.getId() == R.id.btn_2) {
+            setNumber("2");
+        }
+        else if (view.getId() == R.id.btn_3) {
+            setNumber("3");
+        }
+        else if (view.getId() == R.id.btn_4) {
+            setNumber("4");
+        }
+        else if (view.getId() == R.id.btn_5) {
+            setNumber("5");
+        }
+        else if (view.getId() == R.id.btn_5) {
+            setNumber("5");
+        }
+        else if (view.getId() == R.id.btn_6) {
+            setNumber("6");
+        }
+        else if (view.getId() == R.id.btn_7) {
+            setNumber("7");
+        }
+        else if (view.getId() == R.id.btn_8) {
+            setNumber("8");
+        }
+        else if (view.getId() == R.id.btn_9) {
+            setNumber("9");
+        }
+        else if (view.getId() == R.id.btn_0) {
+            setNumber("0");
+        }
+        else if (view.getId() == R.id.btn_dot) {
+            setNumber(".");
+        }
+    }
+
+    public void onOperationClick(View view){
+        if(view.getId() == R.id.btn_ac){
+            textView.setText("0");
+            first = 0.0;
+            second = 0.0;
+        }
+        else if (view.getId() == R.id.btn_plus) {
+            operation = "+";
+            first = Double.parseDouble(textView.getText().toString());
+            isOperationClick = true;
+        }
+        else if (view.getId() == R.id.btn_equal) {
+            second = Double.parseDouble(textView.getText().toString());
+            if(operation.equals("+")){
+                result = first+second;
+                textView.setText(cancelDouble(result));
+            } else if (operation.equals("-")) {
+                result = first-second;
+                textView.setText(cancelDouble(result));
+            }else if (operation.equals("/")) {
+                if(second == 0){
+                    textView.setText("На 0 нельзя");
+                }else{
+                result = first/second;
+                    textView.setText(cancelDouble(result));}
+            }else if (operation.equals("*")) {
+                result = first*second;
+                textView.setText(cancelDouble(result));
+            }
+            else if (operation.equals("%")) {
+                result = (first/100)*second;
+                textView.setText(cancelDouble(result));
+            }
+            isOperationClick = true;
+
+        }
+        else if (view.getId() == R.id.btn_minus) {
+            operation = "-";
+            first = Double.parseDouble(textView.getText().toString());
+            isOperationClick = true;
+        }
+        else if (view.getId() == R.id.btn_multiplication) {
+            operation = "*";
+            first = Double.parseDouble(textView.getText().toString());
+            isOperationClick = true;
+        }
+        else if (view.getId() == R.id.btn_division) {
+            operation = "/";
+            first = Double.parseDouble(textView.getText().toString());
+            isOperationClick = true;
+        }
+        else if (view.getId() == R.id.btn_percent) {
+            operation = "%";
+            first = Double.parseDouble(textView.getText().toString());
+            isOperationClick = true;
+        }
+
+    }
+
+    public String cancelDouble(Double number){
+        String text = number.toString();
+        if(text.substring(text.length() - 2).equals(".0")){
+            return text.substring(0,text.length() - 2);
+        }
+        else{
+            return number.toString();
+        }
+    }
+
+
+    public void setNumber(String number){
+        if(textView.getText().toString().equals("0")){
+            textView.setText(number);
+        } else if (isOperationClick) {
+            textView.setText(number);
+        } else{
+            textView.append(number);
+        }
+        isOperationClick = false;
+    }
+}
